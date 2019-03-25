7da6a5fc35100c27af676599d03636118d235070
xEdwardBlom1337 <fjuliusa@gmail.com>
2019-03-25 11:00:25 +0100

    Better room codes generated

--- tools.js
+++ tools.js
@@ -49,11 +49,18 @@ function indexOf(element, arr) {
     return null;
 }
 
-let x = 0;
+function randInt(min, max) {
+    return Math.floor(Math.random() * (max - min + 1) + min);
+}
+
 class Room {
     constructor() {
-        this.roomCode = 'abc' + x;
-        x++;
+        this.roomCode = String.fromCharCode(
+            randInt(48,91),
+            randInt(48,91),
+            randInt(48,91),
+            randInt(48,91)
+            );
         this.players = [];
         this.round = 0;
         this.tiles = [];
